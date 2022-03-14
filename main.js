0e8d3118e48c23e5d739c30181c8f13c9c255502
Romain Chevalier <r.chevalier871@gmail.com>
2022-03-14 15:19:58 +0100

    exo3

--- main.js
+++ main.js
@@ -3,3 +3,9 @@
 const helloWorld = <h1>Hello world!</h1>;
 
 ReactDOM.render(helloWorld, document.querySelector('#app'));
+
+const lastName = <span className="red uppercase">Romain</span>;
+const firstName= <span className="firstUppercase">Chevalier</span>   ;
+
+const all = <span>{lastName} {firstName}</span>;
+ReactDOM.render(all, document.querySelector('#app'));
