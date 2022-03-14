357863759649659d48c92fffdd98e6e286ea87ae
Romain Chevalier <r.chevalier871@gmail.com>
2022-03-14 17:29:39 +0100

    exo6

--- main.js
+++ main.js
@@ -1,80 +1,3 @@
-/* 
-// Étape 1
-class Clock extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>Bonjour, monde !</h1>
-          <h2>Il est {this.props.date.toLocaleTimeString()}.</h2>
-        </div>
-      );
-    }
-  }
-
-ReactDOM.render(<Clock date={new Date()}/>, document.querySelector('#app')); 
-
-*/
-
-
-/* 
-
-// Étape 2
-
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date()};
-    }
-  render() {
-    return (
-      <div>
-        <h1>Bonjour, monde !</h1>
-        <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Clock />, document.querySelector('#app')); 
-
-
- */
-
-
-
-// Étape 3
-
-/* class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { date: new Date()};
-    }
-    componentDidMount() {
-        this.timerID = setInterval(() => this.tick(),1000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        })
-    }
-
-    render() {
-        return (
-        <div>
-            <h1>Bonjour, monde !</h1>
-            <h2>Il est {this.state.date.toLocaleTimeString()}.</h2>
-        </div>
-        );
-    }
-}
-
-ReactDOM.render(<Clock />, document.querySelector('#app'));  */
-
 
 function Clock(props) {
     React.useEffect(() => {
@@ -82,18 +5,29 @@ function Clock(props) {
     }, []);
     
     const [date, setDate] = React.useState(new Date());
+    const [textColor, setTextColor] = React.useState("black");
 
     const tick = () => {
         setInterval(() => {
             setDate(new Date())
         }, 1000)
     }
+    const generateRandomColor = () => {
+            var randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`; 
+            setTextColor(randomColor) ;
+    }
+
+    const resetColor = () => {
+        setTextColor("black");
+    }
     
 
     return (
         <div>
             <h1>Hello world</h1>
-            <h2>Il est {date.toLocaleTimeString()}.</h2>
+            <h2 style={{color: textColor}}>Il est {date.toLocaleTimeString()}.</h2>
+            <button onClick={generateRandomColor}>Changer de couleur</button>
+            <button onClick={resetColor}>Reset couleur</button>
         </div>
         );
 }
